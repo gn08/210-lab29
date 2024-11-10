b506b59d33ccb0a587377cf1a42fe669baeb4c45
gn08 <gabbyfilipova@gmail.com>
2024-11-10 13:49:22 -0800

    commit 1:50

---
+++ main.cpp
@@ -0,0 +1,33 @@
+//include headers std::map, std::array, std::list
+void simulateLibraryOperations(std::map<int, std::array<std::list<std::string>, 3>>& library, int days) {
+	for (int day =1; day <= days; ++day){
+	//output day and status
+	// add, delete, books
+	//check out or return
+	//save data to history
+	//display changes
+	}
+}
+
+int main() {
+    std::map<int, std::array<std::list<std::string>, 3>> library; 
+	//get the data from history
+	//show the number for book, author name, title name, genre, year published, and availability
+	while (true) {
+	//display menu options
+	//collect the input
+		switch (userChoice){
+			Case 1:
+				//new book
+			Case 2:
+				//delete book
+			Case 3:
+				//search for book
+			Case 4:
+				//list books
+			Case 5:
+				//quit/stop
+				return 0;
+	}
+	return 0;
+}
